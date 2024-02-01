0b6ba6d038cf829672251b4838ef8e2f0b47153e
Joselyn <143128430+DURREJOS@users.noreply.github.com>
2024-01-31 18:00:04 -0700

    TempConV2

--- src/TemperatureConversion.java
+++ src/TemperatureConversion.java
@@ -1,5 +1,18 @@
-public class Main {
+import java.util.Scanner; // imports code for me to use the scanner
+
+public class TemperatureConversion {
     public static void main(String[] args) {
-        System.out.println("HeLLO WoRLd!");
+
+                Scanner in = new Scanner(System.in); // is used for input
+
+        System.out.println("Give me any temperature in Fahrenheit!"); // given input
+
+        int fahrenheit = in.nextInt(); // connects the input to the scanner
+
+        double CelsiusConverter = (fahrenheit - 32) * 5/9; // double is used for
+        double kelvinConverter = (fahrenheit - 32) * (5/9) - 273.15; // double is used for
+
+        System.out.println( CelsiusConverter + " Degrees Celsius"); // prints output double converters I made
+        System.out.println( kelvinConverter + " kelvin"); // and then adds the words at the end in ""
     }
 }
