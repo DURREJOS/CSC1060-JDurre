dc26485c38cf8ff4848a315a3ff14ec623447766
Joselyn <143128430+DURREJOS@users.noreply.github.com>
2024-02-13 17:07:54 -0700

    CircleCalculationsV2

--- src/CircleCalculations.java
+++ src/CircleCalculations.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class CircleCalculations {
+    public static double findArea(double radius){
+        return radius * radius*Math.PI;
+    }
+    public static double findCircumference(double radius){
+        return radius*Math.PI;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the radius of the circle:");
         int radius = in.nextInt();
 
-        int diameter = radius * 2;
-        double area = (radius* radius) * Math.PI;
-        double circumference = radius * 2 * Math.PI;
-
+        double diameter = radius*2;
+        double area = findArea(diameter/2);
+        double circumference = findCircumference(diameter);
         System.out.println("The diameter of the circle is "+ diameter + ".");
         System.out.println("The area of the circle is "+ area + ".");
         System.out.println("The circumference of the circle is "+ circumference + ".");
