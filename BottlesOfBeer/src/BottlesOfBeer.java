20ae5adcde2bce0e6a0e3e364f8b704ce6c02745
Joselyn <143128430+DURREJOS@users.noreply.github.com>
2024-02-19 23:44:09 -0700

    TrianglesV1

--- BottlesOfBeer/src/BottlesOfBeer.java
+++ BottlesOfBeer/src/BottlesOfBeer.java
@@ -9,24 +9,24 @@ public class BottlesOfBeer {
         System.out.println("Enter the number of bottles to start with:");
         int verse = in.nextInt();
         int bottleNum = verse;
-        while (bottleNum > 0) {
+        while (bottleNum > 0) { // while loop
 
-            if (bottleNum == 1) {
-                System.out.println();
+            if (bottleNum == 1) { // each if statement has different requirements in countdown
+                System.out.println(); // This creates spaces between what is printed
                 System.out.println(bottleNum + " bottle of beer on the wall, " + bottleNum + " bottle of beer.");
                 System.out.println("Take one down and pass it around, " + bottleNum + " bottle of beer on the wall.");
                 bottleNum = bottleNum - 1;
             }
 
             if (bottleNum <= 99) {
-                System.out.println();
+                System.out.println(); // This creates spaces between what is printed
                 System.out.println(bottleNum + " bottles of beer on the wall, " + bottleNum + " bottles of beer.");
                 System.out.println("Take one down and pass it around, " + bottleNum + " bottles of beer on the wall.");
                 bottleNum = bottleNum - 1;
             }
 
         }
-        System.out.println();
+        System.out.println(); // This creates spaces between what is printed
         System.out.println("Go to the store and buy some more, " + verse + " bottles of beer on the wall.");
     }
 }
