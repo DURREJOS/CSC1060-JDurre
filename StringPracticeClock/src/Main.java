a07a06be981e48a7abe8f990409e9a651a99f5f3
Joselyn <143128430+DURREJOS@users.noreply.github.com>
2024-03-01 18:11:38 -0700

    StringWorkshopV1

--- StringPracticeClock/src/Main.java
+++ StringPracticeClock/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String example = "Some stuff in here";
+        endsWith(example, maybeEnd:"");
+        System.out.println();
+    }
+
+    public static boolean endsWith(String string, String maybeEnd) {
+        return string.endsWith(maybeEnd);
     }
 }
